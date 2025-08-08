41a3db4bf2a12e01b20696ce615b44a15cb52745
shahanaROHANA <kokilarubanshakana@gmail.com>
2025-08-08 09:56:58 +0530

    second change

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ function getName(){
     const saveData=localStorage.getItem(`Name`)
     console.log(saveData)
     const output=document.getElementById("output");
+    document.getElementById("output").innerHTML = "your data is "+ saveData;
 }
 function clearStorage(){
     localStorage.removeItem(`Name`);
 }
+
 
 
 
